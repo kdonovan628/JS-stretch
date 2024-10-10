62597333f3453b8ac188d14b38f28a5075dcf097
kdonovan628 <kelsey.donovan628@gmail.com>
2024-10-10 08:14:07 -0600

    add index.js, index.html

---
+++ index.js
@@ -0,0 +1,6 @@
+// create an index.html file 
+// create an index.js file
+// use variables and console.log to display the words "hello world" in the console
+
+const x = "Hello World"
+console.log ("Hello World")
